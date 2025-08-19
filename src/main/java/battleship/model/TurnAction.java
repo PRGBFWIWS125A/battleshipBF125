120bc8d7bd04dced9f66839e130fd9f2775fafa5
Pascal7790 <Holstein7790@web.de>
2025-08-19 10:58:00 +0200

    TurnAction

--- src/main/java/battleship/model/TurnAction.java
+++ src/main/java/battleship/model/TurnAction.java
@@ -3,4 +3,8 @@ package battleship.model;
 import java.util.function.Function;
 
 public abstract class TurnAction implements Function<EventAndState, Boolean> {
+    public final Player player;
+    public TurnAction(Player player){
+        this.player = player;
+    }
 }
