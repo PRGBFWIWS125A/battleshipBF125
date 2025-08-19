19a643e2a17f5216e8d66f70f9f59e4c2cdb2828
Thomas Ströder <thomas.stroeder@email.de>
2025-08-19 08:29:26 +0100

    skeleton for sheet 4, ex 1 d and e

---
+++ src/main/java/battleship/model/ShipPlacementAction.java
@@ -0,0 +1,15 @@
+package battleship.model;
+
+public class ShipPlacementAction extends TurnAction {
+
+    public ShipPlacementAction(Player player, ShipType type) {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Boolean apply(EventAndState eventAndState) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/main/java/battleship/model/ShotAction.java
@@ -0,0 +1,15 @@
+package battleship.model;
+
+public class ShotAction extends TurnAction {
+
+    public ShotAction(Player player) {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Boolean apply(EventAndState eventAndState) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
