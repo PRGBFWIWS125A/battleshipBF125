c2052a565bd799bc8c0f1f3c94f26061f9bf626b
Thomas Ströder <thomas.stroeder@email.de>
2025-08-26 10:07:45 +0100

    sheet 4 ex 1 e

--- src/main/java/battleship/model/ShotAction.java
+++ src/main/java/battleship/model/ShotAction.java
@@ -2,15 +2,27 @@ package battleship.model;
 
 public class ShotAction extends TurnAction {
 
-    public ShotAction(Player player) {
+    public ShotAction(final Player player) {
         super(player);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
-    public Boolean apply(EventAndState eventAndState) {
-        // TODO Auto-generated method stub
-        return null;
+    public Boolean apply(final EventAndState eventAndState) {
+        return eventAndState.rules().shot(eventAndState.game(), this.player, eventAndState.event());
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o instanceof ShotAction) {
+            final ShotAction other = (ShotAction)o;
+            return this.player == other.player;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.player.hashCode() * 5;
     }
 
 }
